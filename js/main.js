bcfa807819dd48e626fbb87865dc19f17b4e13ae
Holidaydes <holiday.de@gmail.com>
2021-12-29 19:10:43 +0100

    few improvements

--- js/main.js
+++ js/main.js
@@ -1,54 +1,62 @@
 const options = {};
+const allowedUrl = 'https://www.interfood.hu/etlap-es-rendeles/';
 
 chrome.storage.local.get('options', (data) => {
     Object.assign(options, data.options);
-    optionsForm.vegetarian.checked = Boolean(options.vegetarian);
-    optionsForm.mushroom.checked = Boolean(options.mushroom);
-  });
+    optionsForm.vegetarianCheckbox.checked = Boolean(options.vegetarianFilter);
+    optionsForm.mushroomCheckbox.checked = Boolean(options.mushroomFilter);
+});
+
+(async function init() {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    if (tab.url !== allowedUrl) {
+        optionsForm.style.opacity = '0.3';
+        optionsForm.vegetarianCheckbox.disabled = true;
+        optionsForm.mushroomCheckbox.disabled = true;
+        optionsForm.clearButton.disabled = true;
+    }
+})();
 
 function selectVegetarianOptions() {
     Array.from(document.querySelectorAll('[class^="middle-text food-etlap-szoveg"]'))
-    .forEach(e => {
-        if(e.children.length === 0) {
-            e.parentNode.parentNode.parentNode.style.pointerEvents = 'none'
-            e.parentNode.parentNode.parentNode.style.opacity = '0.3'
-        }
-    });
+        .forEach(e => {
+            if (e.children.length === 0) {
+                e.parentNode.parentNode.parentNode.style.pointerEvents = 'none'
+                e.parentNode.parentNode.parentNode.style.opacity = '0.3'
+            }
+        });
 }
 
 function selectMushroom() {
     Array.from(document.querySelectorAll('[class^="middle-text food-etlap-szoveg"]'))
-    .forEach(e => {
-        if(!e.parentNode.parentNode.children.item(1).innerHTML.includes('gomba')) {
-            e.parentNode.parentNode.parentNode.style.pointerEvents = 'none'
-            e.parentNode.parentNode.parentNode.style.opacity = '0.3'
-        }
-    });
+        .forEach(e => {
+            if (!e.parentNode.parentNode.children.item(1).innerHTML.includes('gomba')) {
+                e.parentNode.parentNode.parentNode.style.pointerEvents = 'none'
+                e.parentNode.parentNode.parentNode.style.opacity = '0.3'
+            }
+        });
 }
 
 function clear() {
     Array.from(document.querySelectorAll('[class^="middle-text food-etlap-szoveg"]'))
-    .forEach(e => {
-        e.parentNode.parentNode.parentNode.style.pointerEvents = ''
-        e.parentNode.parentNode.parentNode.style.opacity = ''
-    });
+        .forEach(e => {
+            e.parentNode.parentNode.parentNode.style.pointerEvents = ''
+            e.parentNode.parentNode.parentNode.style.opacity = ''
+        });
 }
 
-optionsForm.vegetarian.addEventListener("change", async function() {
+optionsForm.vegetarianCheckbox.addEventListener("change", async function() {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    
-    options.vegetarian = this.checked;
-    chrome.storage.local.set({options});
+    options.vegetarianFilter = this.checked;
+    chrome.storage.local.set({ options });
 
-    console.log('vegetarian option is checked')
     if (this.checked) {
-        console.log('vegetarian option is checked')
         chrome.scripting.executeScript({
             target: { tabId: tab.id },
             function: selectVegetarianOptions
         });
     } else {
-        console.log('vegetarian option is not checked')
         chrome.scripting.executeScript({
             target: { tabId: tab.id },
             function: clear
@@ -56,24 +64,36 @@ optionsForm.vegetarian.addEventListener("change", async function() {
     }
 });
 
-optionsForm.mushroom.addEventListener("change", async function() {
+optionsForm.mushroomCheckbox.addEventListener("change", async function() {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    options.mushroomFilter = this.checked;
+    chrome.storage.local.set({ options });
 
-    options.mushroom = this.checked;
-    chrome.storage.local.set({options});
-
-    console.log('mushroom option is checked')
     if (this.checked) {
-        console.log('mushroom option is checked')
         chrome.scripting.executeScript({
             target: { tabId: tab.id },
             function: selectMushroom
         });
     } else {
-        console.log('mushroom option is not checked')
         chrome.scripting.executeScript({
             target: { tabId: tab.id },
             function: clear
         });
     }
+});
+
+optionsForm.clearButton.addEventListener("click", async function() {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    optionsForm.vegetarianCheckbox.checked = false;
+    optionsForm.mushroomCheckbox.checked = false;
+
+    options.vegetarianFilter = false;
+    options.mushroomFilter = false;
+    chrome.storage.local.set({ options });
+
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: clear
+    });
 });

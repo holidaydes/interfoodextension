a65ab3705c4459b094c3ecb4df6d82e8f6903f6e
Holidaydes <holiday.de@gmail.com>
2022-01-15 13:32:02 +0100

    filter fix and add new filter options

--- js/main.js
+++ js/main.js
@@ -5,35 +5,64 @@ chrome.storage.local.get('options', (data) => {
     Object.assign(options, data.options);
     optionsForm.vegetarianCheckbox.checked = Boolean(options.vegetarianFilter);
     optionsForm.mushroomCheckbox.checked = Boolean(options.mushroomFilter);
+    optionsForm.paleoCheckbox.checked = Boolean(options.paleoFilter);
+    optionsForm.newMealCheckbox.checked = Boolean(options.newMealFilter);
+    optionsForm.timeLimitedOrderCheckbox.checked = Boolean(options.timeLimitedOrderFilter);
 });
 
 (async function init() {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-    if (tab.url !== allowedUrl) {
+    if (!tab.url.includes(allowedUrl)) {
         optionsForm.style.opacity = '0.3';
         optionsForm.vegetarianCheckbox.disabled = true;
         optionsForm.mushroomCheckbox.disabled = true;
+        optionsForm.paleoCheckbox.disabled = true;
+        optionsForm.newMealCheckbox.disabled = true;
         optionsForm.clearButton.disabled = true;
+        optionsForm.timeLimitedOrderCheckbox.disabled = true;
     }
 })();
 
 function filterCells(options) {
     Array.from(document.querySelectorAll('[id^="sorszuro_id-"] > div.scroll-cols > div.wrapper > div.inner > div.cell'))
-    .forEach(
-        cell => {
-            if (
-                (options.vegetarianFilter && cell.children.item(0).children.item(0).children.item(0).children.length === 0) || 
-                (options.mushroomFilter && !cell.children.item(0).children.item(1).innerHTML.includes('gomba'))
-            ) {
-                cell.style.pointerEvents = 'none';
-                cell.style.opacity = '0.3';
-            } else {
-                cell.style.pointerEvents = ''
-                cell.style.opacity = ''
+        .forEach(
+            cell => {
+                if (
+                    //vegetarian filter
+                    (
+                        options.vegetarianFilter &&
+                        Array.from(cell.children.item(0).children.item(0).children.item(0).childNodes).filter(node => node.nodeName === 'SPAN' && node.innerHTML === '<b>V</b>').length === 0
+                    ) ||
+                    //mushroom filter
+                    (
+                        options.mushroomFilter &&
+                        !cell.children.item(0).children.item(1).innerHTML.includes('gomba')
+                    ) ||
+                    //paleo filter
+                    (
+                        options.paleoFilter &&
+                        Array.from(cell.children.item(0).children.item(0).children.item(0).childNodes).filter(node => node.nodeName === 'IMG' && node.title === 'Paleo').length === 0
+                    ) ||
+                    //new meal filter
+                    (
+                        options.newMealFilter &&
+                        Array.from(cell.children.item(0).children.item(0).children.item(0).childNodes).filter(node => node.nodeName === 'IMG' && node.title.includes('J!')).length === 0
+                    ) ||
+                    //time limited order filter
+                    (
+                        options.timeLimitedOrderFilter &&
+                        Array.from(cell.children.item(0).children.item(0).children.item(0).childNodes).filter(node => node.nodeName === 'IMG' && node.title.includes('rendel')).length === 0
+                    )
+                ) {
+                    cell.style.pointerEvents = 'none';
+                    cell.style.opacity = '0.3';
+                } else {
+                    cell.style.pointerEvents = ''
+                    cell.style.opacity = ''
+                }
             }
-        }
-    );
+        );
 }
 
 function clear() {
@@ -49,6 +78,9 @@ optionsForm.vegetarianCheckbox.addEventListener("change", async function() {
 
     options.vegetarianFilter = optionsForm.vegetarianCheckbox.checked;
     options.mushroomFilter = optionsForm.mushroomCheckbox.checked;
+    options.paleoFilter = optionsForm.paleoCheckbox.checked;
+    options.newMealFilter = optionsForm.newMealCheckbox.checked;
+    options.timeLimitedOrderFilter = optionsForm.timeLimitedOrderCheckbox.checked;
 
     chrome.storage.local.set({ options });
 
@@ -64,6 +96,63 @@ optionsForm.mushroomCheckbox.addEventListener("change", async function() {
 
     options.vegetarianFilter = optionsForm.vegetarianCheckbox.checked;
     options.mushroomFilter = optionsForm.mushroomCheckbox.checked;
+    options.paleoFilter = optionsForm.paleoCheckbox.checked;
+    options.newMealFilter = optionsForm.newMealCheckbox.checked;
+    options.timeLimitedOrderFilter = optionsForm.timeLimitedOrderCheckbox.checked;
+
+    chrome.storage.local.set({ options });
+
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: filterCells,
+        args: [options]
+    });
+});
+
+optionsForm.paleoCheckbox.addEventListener("change", async function() {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    options.vegetarianFilter = optionsForm.vegetarianCheckbox.checked;
+    options.mushroomFilter = optionsForm.mushroomCheckbox.checked;
+    options.paleoFilter = optionsForm.paleoCheckbox.checked;
+    options.newMealFilter = optionsForm.newMealCheckbox.checked;
+    options.timeLimitedOrderFilter = optionsForm.timeLimitedOrderCheckbox.checked;
+
+    chrome.storage.local.set({ options });
+
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: filterCells,
+        args: [options]
+    });
+});
+
+optionsForm.newMealCheckbox.addEventListener("change", async function() {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    options.vegetarianFilter = optionsForm.vegetarianCheckbox.checked;
+    options.mushroomFilter = optionsForm.mushroomCheckbox.checked;
+    options.paleoFilter = optionsForm.paleoCheckbox.checked;
+    options.newMealFilter = optionsForm.newMealCheckbox.checked;
+    options.timeLimitedOrderFilter = optionsForm.timeLimitedOrderCheckbox.checked;
+
+    chrome.storage.local.set({ options });
+
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: filterCells,
+        args: [options]
+    });
+});
+
+optionsForm.timeLimitedOrderCheckbox.addEventListener("change", async function() {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    options.vegetarianFilter = optionsForm.vegetarianCheckbox.checked;
+    options.mushroomFilter = optionsForm.mushroomCheckbox.checked;
+    options.paleoFilter = optionsForm.paleoCheckbox.checked;
+    options.newMealFilter = optionsForm.newMealCheckbox.checked;
+    options.timeLimitedOrderFilter = optionsForm.timeLimitedOrderCheckbox.checked;
 
     chrome.storage.local.set({ options });
 
@@ -79,9 +168,15 @@ optionsForm.clearButton.addEventListener("click", async function() {
 
     optionsForm.vegetarianCheckbox.checked = false;
     optionsForm.mushroomCheckbox.checked = false;
+    optionsForm.paleoCheckbox.checked = false;
+    optionsForm.newMealCheckbox.checked = false;
+    optionsForm.timeLimitedOrderCheckbox.checked = false;
 
     options.vegetarianFilter = false;
     options.mushroomFilter = false;
+    options.paleoFilter = false;
+    options.newMealFilter = false;
+    options.timeLimitedOrderFilter = false;
 
     chrome.storage.local.set({ options });
 
